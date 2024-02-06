adfa775918bedac61fb33e6e81966e885083026e
Paulo Henrique Sousa da Silva <pauloss@ciandt.com>
2024-02-06 13:58:36 -0300

    Create a new endpoint

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (_, res) => 
     res.status(200).send({ result: 'Server OK!!' }));
 
+app.get('/api/user', (_, res) =>
+	res.status(200).send({ name: "Test User", id: 9999 }));
+
 module.exports = {
     app
 };
